4e37ba564b8f46506613d32fde9e6d7aa63c985d
JimHaff <james.haff@generalassemb.ly>
2024-03-11 15:00:17 -0500

    Added signup

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,8 +1,84 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+export default function SignUpPage() {
 
-export default function SignUpPage(){
-	return (
-		<h1>Signup page here</h1>
-	)
+	const [state, setState] = useState({
+		username: '',
+		email: '',
+		password: '',
+		passwordConf: '',
+		bio: ''
+	})
+
+	function handleChange(e){
+
+		setState({
+			...state,
+			[e.target.name]: e.target.value
+		})
+	}
+
+  return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="purple" textAlign="center">
+          <Image src="https://i.imgur.com/TM4eA5g.jpg" /> Sign Up
+        </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <Form.TextArea
+              label="bio"
+              name="bio"
+			  value={state.bio}
+              placeholder="Tell us more about your dogs..."
+              onChange={handleChange}
+            />
+            <Form.Field>
+              <Form.Input
+                type="file"
+                name="photo"
+                placeholder="upload image"
+                onChange={handleFileInput}
+              />
+            </Form.Field>
+            <Button type="submit" className="btn">
+              Signup
+            </Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
 }
