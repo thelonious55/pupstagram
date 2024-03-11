9c155fcc07c4069601f469076a5c72fefc9b581b
JimHaff <james.haff@generalassemb.ly>
2024-03-11 14:38:50 -0500

    ADded code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import SignUpPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+
+// ANY component that is rendered by a route, should be stored in the 
+// pages folder. Every page is like an app component
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
     </Routes>
   );
 }
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,1 +1,8 @@
+import { useState } from 'react'
 
+
+export default function SignUpPage(){
+	return (
+		<h1>Signup page here</h1>
+	)
+}
