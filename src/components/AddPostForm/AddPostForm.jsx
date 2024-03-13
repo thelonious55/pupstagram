55b8207271c773ef45fa9dd4eb0534050445c20e
JimHaff <james.haff@generalassemb.ly>
2024-03-13 13:15:01 -0500

    Added code

--- src/components/AddPostForm/AddPostForm.jsx
+++ src/components/AddPostForm/AddPostForm.jsx
@@ -1,6 +1,53 @@
+import { useState } from 'react'
+import { Button, Form, Segment } from 'semantic-ui-react'
+
+
 export default function AddPostForm(){
     
+    const [state, setState] = useState({
+        caption: ''
+    })
+
+    const [photo, setPhoto] = useState({})
+
+    function handleFileInput(e){
+        setPhoto(e.target.files[0])
+    }
+
+    function handleChange(e){
+        setState({
+            ...state, 
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit(){
+        
+    }
+
     return (
-        <span> Post Form</span>
-    )
+    <Segment>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Form.Input
+            className="form-control"
+            name="caption"
+            value={state.caption}
+            placeholder="What's on your pups mind?"
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+            className="form-control"
+            type="file"
+            name="photo"
+            placeholder="upload image"
+            onChange={handleFileInput}
+          />
+          <Button type="submit" className="btn">
+            ADD PUPPY
+          </Button>
+        </Form>
+      </Segment>
+ 
+    );
 }
--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,6 +1,27 @@
+import PostFeed from "../../components/PostFeed/PostFeed";
+import Header from "../../components/Header/Header";
+import AddPostForm from "../../components/AddPostForm/AddPostForm";
 
-export default function FeedPage(){
-	return (
-		<h1>This is the Feed Page</h1>
-	)
+import { Grid } from "semantic-ui-react";
+
+export default function FeedPage() {
+  return (
+    <Grid centered>
+      <Grid.Row>
+        <Grid.Column>
+          <Header />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <AddPostForm />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <PostFeed />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
